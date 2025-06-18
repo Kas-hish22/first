4941f703a1fb13fdaec0839044d02d5f827c91e1
Kas-hish22 <engr.asifraazapanhwar@gmail.com>
2025-06-18 12:42:30 +0500

    Calculator

--- h.py
+++ h.py
@@ -1,1 +1,14 @@
-print("Hello")
+from utils import add, subtract, multiply, divide
+
+def main():
+    print("Simple Calculator")
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+
+    print(f"Add: {add(a, b)}")
+    print(f"Subtract: {subtract(a, b)}")
+    print(f"Multiply: {multiply(a, b)}")
+    print(f"Divide: {divide(a, b)}")
+
+if __name__ == "__main__":
+    main()
